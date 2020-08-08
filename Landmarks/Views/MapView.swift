f726f25be45b0cd2e8ce2b789ffea11a04eca779
dgwu <danielgunawanwu@gmail.com>
2020-08-08 23:59:39 +0700

    first commit and completion of SwiftUI Essentials, Creating and Combining Views

---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Daniel Gunawan on 08/08/20.
+//  Copyright © 2020 Daniel Gunawan. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
